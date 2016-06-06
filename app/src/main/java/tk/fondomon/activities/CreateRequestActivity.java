36373209fda03046d8c90dd617cbdb448b3871a2
Miguel <cmiguelmg@gmail.com>
2016-06-06 12:36:34 -0500

    looking notifications, and create request' design is done.

--- app/src/main/java/tk/fondomon/activities/CreateRequestActivity.java
+++ app/src/main/java/tk/fondomon/activities/CreateRequestActivity.java
@@ -1,30 +1,58 @@
 package tk.fondomon.activities;
 
+import android.annotation.TargetApi;
+import android.app.AlertDialog;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.ProgressDialog;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import tk.fondomon.entities.SmfMember;
 
 public class CreateRequestActivity extends AppCompatActivity {
 
     private String fee;
     private String payment;
 
+    private EditText completeName;
+    private EditText money;
+    private EditText time;
+    private EditText disbursement;
+    private EditText infAdditional;
+    private Spinner spinnerFee;
+    private Spinner spinnerPayment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_request);
 
-        Spinner spinnerFee = (Spinner)findViewById(R.id.spinnerFee);
+        completeName = (EditText)findViewById(R.id.completeName);
+        money = (EditText)findViewById(R.id.money_requested);
+        time = (EditText)findViewById(R.id.time_limit);
+        disbursement = (EditText)findViewById(R.id.disbursement);
+        infAdditional = (EditText)findViewById(R.id.inf_additional);
+        // ------------------------------------------------------------------
+        spinnerFee = (Spinner)findViewById(R.id.spinnerFee);
         spinnerFee.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -48,7 +76,7 @@ public class CreateRequestActivity extends AppCompatActivity {
 
         spinnerFee.setAdapter(dataAdapterFee);
         // ------------------------------------------------------------------
-        Spinner spinnerPayment = (Spinner)findViewById(R.id.spinnerPayment);
+        spinnerPayment = (Spinner)findViewById(R.id.spinnerPayment);
         spinnerPayment.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -73,6 +101,12 @@ public class CreateRequestActivity extends AppCompatActivity {
         spinnerPayment.setAdapter(dataAdapterPayment);
         // ------------------------------------------------------------------
         Button sendButton = (Button)findViewById(R.id.button_save);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendRequest();
+            }
+        });
     }
 
     @Override
@@ -91,4 +125,147 @@ public class CreateRequestActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void sendRequest(){
+        String name = completeName.getText().toString();
+        String moneyStr = money.getText().toString();
+        String timeStr = time.getText().toString();
+        String disbursementStr = disbursement.getText().toString();
+        String inf = infAdditional.getText().toString();
+
+        View focusView = null;
+        boolean cancel = false;
+
+        if(TextUtils.isEmpty(name)){
+            completeName.setError(getString(R.string.error_field_required));
+            focusView = completeName;
+            cancel = true;
+        }else if(TextUtils.isEmpty(moneyStr)){
+            money.setError(getString(R.string.error_field_required));
+            focusView = money;
+            cancel = true;
+        }else if(TextUtils.isEmpty(timeStr)){
+            time.setError(getString(R.string.error_field_required));
+            focusView = time;
+            cancel = true;
+        } else if(fee.equals(getString(R.string.fee))){
+            showMessage("Error", getString(R.string.error_field_fee));
+            focusView = spinnerFee;
+            cancel = true;
+        } else if(TextUtils.isEmpty(disbursementStr)){
+            disbursement.setError(getString(R.string.error_field_required));
+            focusView = disbursement;
+            cancel = true;
+        } else if(payment.equals(getString(R.string.payment))){
+            showMessage("Error", getString(R.string.error_field_payment));
+            focusView = spinnerPayment;
+            cancel = true;
+        }
+
+        if(cancel)
+            focusView.requestFocus();
+        else{
+            showProgress(getString(R.string.msg_sending),true);
+            PublishRequestTask publishTask = new PublishRequestTask(null);
+            publishTask.execute();
+            // Connection with the server, authentication
+
+            NotificationCompat.Builder mBuilder =
+                    new NotificationCompat.Builder(this)
+                            .setSmallIcon(R.drawable.ic_menu_send)
+                            .setContentTitle("My notification")
+                            .setContentText("Hello World!");
+            // Creates an explicit intent for an Activity in your app
+            Intent resultIntent = new Intent(this, MainActivity.class);
+
+            // The stack builder object will contain an artificial back stack for the
+            // started Activity.
+            // This ensures that navigating backward from the Activity leads out of
+            // your application to the Home screen.
+            TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+            // Adds the back stack for the Intent (but not the Intent itself)
+            stackBuilder.addParentStack(MainActivity.class);
+            // Adds the Intent that starts the Activity to the top of the stack
+            stackBuilder.addNextIntent(resultIntent);
+            PendingIntent resultPendingIntent =
+                    stackBuilder.getPendingIntent(
+                            0,
+                            PendingIntent.FLAG_UPDATE_CURRENT
+                    );
+            mBuilder.setContentIntent(resultPendingIntent);
+            NotificationManager mNotificationManager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            // mId allows you to update the notification later on.
+            mNotificationManager.notify(1, mBuilder.build());
+        }
+    }
+
+    /**
+     * show a message in the actual context
+     */
+    public void showMessage( String nameError, String detail) {
+        AlertDialog alert = new AlertDialog.Builder(CreateRequestActivity.this).create();
+        alert.setTitle(nameError);
+        alert.setMessage(detail);
+        alert.show();
+    }
+
+    /**
+     * show the progress of the current task
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    public void showProgress(String message, boolean show){
+        ProgressDialog progress = ProgressDialog.show(CreateRequestActivity.this, null, message, true);
+        if(!show)
+            progress.dismiss();
+    }
+
+    /**
+     * Represents an asynchronous publish request task used to authenticate
+     * the user.
+     */
+    public class PublishRequestTask extends AsyncTask<Void, Void, Boolean> {
+
+        private final SmfMember user;
+
+        PublishRequestTask(SmfMember user) {
+            this.user = user;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            // TODO: attempt authentication against a network service.
+
+            try {
+                // Simulate network access.
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                return false;
+            }
+
+            // TODO: register the new account here.
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            showProgress(null, false);
+            try {
+                if (success) {
+                    showMessage(getString(R.string.msg_notify_title), getString(R.string.msg_notify));
+                    Thread.sleep(2000);
+                    Intent intent = new Intent(CreateRequestActivity.this, MainActivity.class);
+                    startActivity(intent);
+                } else {
+                    showMessage(getString(R.string.error_connection_failed), getString(R.string.error_authentication));
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            showProgress(null, false);
+        }
+    }
 }
--- app/src/main/java/tk/fondomon/activities/LoginActivity.java
+++ app/src/main/java/tk/fondomon/activities/LoginActivity.java
@@ -72,7 +72,7 @@ public class LoginActivity extends AppCompatActivity {
     /**
      * show a message in the actual context
      */
-    public void showError( String nameError, String detail) {
+    public void showMessage( String nameError, String detail) {
         AlertDialog alert = new AlertDialog.Builder(LoginActivity.this).create();
         alert.setTitle(nameError);
         alert.setMessage(detail);
@@ -126,7 +126,7 @@ public class LoginActivity extends AppCompatActivity {
                 Intent intent = new Intent(LoginActivity.this,MainActivity.class);
                 startActivity(intent);
             } else {
-                showError(getString(R.string.error_connection_failed), getString(R.string.error_authentication));
+                showMessage(getString(R.string.error_connection_failed), getString(R.string.error_authentication));
             }
         }
 
